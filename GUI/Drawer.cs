fd5625ba684427464617a826e46a5e87203967cb
Artikty <artyom.chirkov.2013@mail.ru>
2024-03-09 18:21:15 +0700

    Заливка
    Добавлено понятие заливки (без возможности выбрать цвет, пока что заливка чисто зелёная)

--- GUI/Drawer.cs
+++ GUI/Drawer.cs
@@ -10,64 +10,71 @@ using System.Windows.Shapes;
 
 namespace GUI
 {
-	internal class Drawer
-	{
-		static List<Figure> Figures = new List<Figure>();
-		static int indFigures = -1;
+    internal class Drawer
+    {
+        static List<Figure> Figures = new List<Figure>();
+        static int indFigures = -1;
 
-		public static void draw(Canvas canvas)
-		{
-			SolidColorBrush br;
-			for (int i = indFigures; i < Figures.Count; i++)
-			{
-				switch (Figures[i].type)
-				{
-					case "line":
-						Line ln = new Line();
-						ln.X1 = Figures[i].points[0].x;
-						ln.Y1 = Figures[i].points[0].y;
-						ln.X2 = Figures[i].points[1].x;
-						ln.Y2 = Figures[i].points[1].y;
-						br = new SolidColorBrush(Color.FromRgb(0, 0, 0));
-						ln.Stroke = br;
-						ln.StrokeThickness = Figures[i].rectWidth;
-						canvas.Children.Add(ln);
-						break;
-					case "ellipse":
-						Ellipse el = new Ellipse();
-						br = new SolidColorBrush(Color.FromRgb(0, 0, 0));
-						el.Stroke = br;
-						Canvas.SetLeft(el, Math.Min(Figures[i].points[0].x, Figures[i].points[1].x));
-						Canvas.SetTop(el, Math.Min(Figures[i].points[0].y, Figures[i].points[1].y));
-						el.Width = Math.Abs(Figures[i].points[1].x - Figures[i].points[0].x);
-						el.Height = Math.Abs(Figures[i].points[1].y - Figures[i].points[0].y);
-						el.StrokeThickness = Figures[i].rectWidth;
-						canvas.Children.Add(el);
-						break;
-					case "rectangle":
-						Rectangle r = new Rectangle();
-						br = new SolidColorBrush(Color.FromRgb(0, 0, 0));
-						r.Stroke = br;
-						Canvas.SetLeft(r, Math.Min(Figures[i].points[0].x, Figures[i].points[1].x));
-						Canvas.SetTop(r, Math.Min(Figures[i].points[0].y, Figures[i].points[1].y));
-						r.Width = Math.Abs(Figures[i].points[1].x - Figures[i].points[0].x);
-						r.Height = Math.Abs(Figures[i].points[1].y - Figures[i].points[0].y);
-						r.StrokeThickness = Figures[i].rectWidth;
-						canvas.Children.Add(r);
-						break;
-				}
-			}
-		}
-		public static void addFigure(Point p1, Point p2, string selectedType, Canvas canvas)
-		{
-			Figure fig1 = new Figure();
-			fig1.type = selectedType;
-			fig1.points = new List<Point>() { p1, p2 };
-			fig1.rectWidth = 2.0;
-			Figures.Add(fig1);
-			indFigures++;
-			draw(canvas);
-		}
+        public static void draw(Canvas canvas)
+        {
+            SolidColorBrush brStroke;
+            SolidColorBrush brFill;
+            for (int i = indFigures; i < Figures.Count; i++)
+            {
+                switch (Figures[i].type)
+                {
+                    case "line":
+                        Line ln = new Line();
+                        ln.X1 = Figures[i].points[0].x;
+                        ln.Y1 = Figures[i].points[0].y;
+                        ln.X2 = Figures[i].points[1].x;
+                        ln.Y2 = Figures[i].points[1].y;
+                        brStroke = new SolidColorBrush(Color.FromRgb(Figures[i].rectFill_R, Figures[i].rectFill_G, Figures[i].rectFill_B));
+                        ln.Stroke = brStroke;
+                        ln.StrokeThickness = Figures[i].rectWidth;
+                        canvas.Children.Add(ln);
+                        break;
+                    case "ellipse":
+                        Ellipse el = new Ellipse();
+                        brStroke = new SolidColorBrush(Color.FromRgb(Figures[i].rectFill_R, Figures[i].rectFill_G, Figures[i].rectFill_B));
+                        brFill = new SolidColorBrush(Color.FromRgb(Figures[i].fill_R, Figures[i].fill_G, Figures[i].fill_B));
+                        el.Stroke = brStroke;
+                        el.Fill = brFill;
+                        Canvas.SetLeft(el, Math.Min(Figures[i].points[0].x, Figures[i].points[1].x));
+                        Canvas.SetTop(el, Math.Min(Figures[i].points[0].y, Figures[i].points[1].y));
+                        el.Width = Math.Abs(Figures[i].points[1].x - Figures[i].points[0].x);
+                        el.Height = Math.Abs(Figures[i].points[1].y - Figures[i].points[0].y);
+                        el.StrokeThickness = Figures[i].rectWidth;
+                        canvas.Children.Add(el);
+                        break;
+                    case "rectangle":
+                        Rectangle r = new Rectangle();
+                        brStroke = new SolidColorBrush(Color.FromRgb(Figures[i].rectFill_R, Figures[i].rectFill_G, Figures[i].rectFill_B));
+                        brFill = new SolidColorBrush(Color.FromRgb(Figures[i].fill_R, Figures[i].fill_G, Figures[i].fill_B));
+                        r.Stroke = brStroke;
+                        r.Fill = brFill;
+                        Canvas.SetLeft(r, Math.Min(Figures[i].points[0].x, Figures[i].points[1].x));
+                        Canvas.SetTop(r, Math.Min(Figures[i].points[0].y, Figures[i].points[1].y));
+                        r.Width = Math.Abs(Figures[i].points[1].x - Figures[i].points[0].x);
+                        r.Height = Math.Abs(Figures[i].points[1].y - Figures[i].points[0].y);
+                        r.StrokeThickness = Figures[i].rectWidth;
+                        canvas.Children.Add(r);
+                        break;
+                }
+            }
+        }
+        public static void addFigure(Point p1, Point p2, string selectedType, Canvas canvas)
+        {
+            Figure fig1 = new Figure();
+            fig1.type = selectedType;
+            fig1.points = new List<Point>() { p1, p2 };
+            fig1.rectWidth = 2.0;
+            fig1.setFill(50, 255, 10);
+            fig1.setRectFill(0, 0, 0);
+            Figures.Add(fig1);
+            indFigures++;
+            draw(canvas);
+        }
 
-	}
+    }
 }
--- Geometry/Figure.cs
+++ Geometry/Figure.cs
@@ -4,36 +4,34 @@ using System.Drawing;
 
 namespace Geometry
 {
-	public class Point
-	{
-		public int x, y;
-		public Point(int X, int Y)
-		{
-			x = X; y = Y;
-		}
-	}
-
-	interface IFigure
-	{
-		void setPoints(List<Point> Points);
-		void setAngle(double Angle);
-		void setFill(Color Fill);
-		void setRectFill(Color RectFill);
-		void setRectWidth(double RectWidth);
-	}
-	public class Figure : IFigure
-	{
-		public List<Point> points;  // Точки
-		public double angle;        // (0; 2pi] - угол поворота
-		public string type;         // Тип фигуры (эллипс, прямая, ломанная)
-		public Color fill;          // Цвет заливки
-		public Color rectFill;      // Цвет рамки
-		public double rectWidth;    // Толщина рамки
-		void IFigure.setPoints(List<Point> Points) { this.points = Points; }
-		void IFigure.setAngle(double Angle) { this.angle = Angle; }
-		void IFigure.setFill(Color Fill) { this.fill = Fill; }
-		void IFigure.setRectFill(Color RectFill) { this.rectFill = RectFill; }
-		void IFigure.setRectWidth(double RectWidth) { this.rectWidth = RectWidth; }
-	}
-
+    public class Point
+    {
+        public string colorName;      // Имя цвета
+        public int r, g, b;           // Значения RGB цвета
+        public int x, y;
+        public Point(int X, int Y)
+        {
+            x = X; y = Y;
+        }
+    }
+    public class Figure
+    {
+        public List<Point> points;  // Точки
+        public double angle;        // (0; 2pi] - угол поворота
+        public string type;         // Тип фигуры (эллипс, прямая, ломанная)
+        public byte fill_R, fill_G, fill_B;                     // Цвет заливки
+        public byte rectFill_R, rectFill_G, rectFill_B;         // Цвет рамки
+        public double rectWidth;                                // Толщина рамки
+        public void setPoints(List<Point> Points) { this.points = Points; }
+        public void setAngle(double Angle) { this.angle = Angle; }
+        public void setRectWidth(double RectWidth) { this.rectWidth = RectWidth; }
+        public void setFill(byte R, byte G, byte B)
+        {
+            fill_R = R; fill_G = G; fill_B = B;
+        }
+        public void setRectFill(byte R, byte G, byte B)
+        {
+            rectFill_R = R; rectFill_G = G; rectFill_B = B;
+        }
+    }
 }
