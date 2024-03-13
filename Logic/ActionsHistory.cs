a400717e40486aa716e5221057690a9ad31faa4a
Aptecc08 <Aptecc08@msil.ru>
2024-03-13 16:26:03 +0700

    +History

---
+++ Logic/ActionsHistory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class ActionsHistory
+    {
+        private static ActionsHistory instance;
+
+        private List<DrawingAction> drawingActions;
+
+        private ActionsHistory()
+        {
+            drawingActions = new List<DrawingAction>();
+        }
+
+        public static ActionsHistory getInstance()
+        {
+            if (instance == null)
+                instance = new ActionsHistory();
+            return instance;
+        }
+    }
+}
