e8c6e23aa4aa70fec52b75590faa5f09dbd983fd
pavlik <71833051+paulusses@users.noreply.github.com>
2024-03-20 22:58:10 +0700

    тест
    текст

--- Geometry/obj/Debug/net472/Geometry.AssemblyInfo.cs
+++ Geometry/obj/Debug/net472/Geometry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Geometry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437bac96f6e51c6ab5da4dc07753bb3879f1a99f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Geometry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Geometry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
